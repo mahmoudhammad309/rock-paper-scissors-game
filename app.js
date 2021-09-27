677e53de1be27ee3d7332c6e2b4dbd4714bebdb0
Mahmoud Hammad <0597694093mahmoud@gmail.com>
2021-09-27 16:55:13 +0300

    create a computer choose function

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const srartBtn = document.querySelector('.start-btn');
+const computerIMG = document.getElementById('computer-img');
+const playerIMG = document.getElementById('player-img');
+ 
 
 
 
 
 
+const computerOptions = ['rock_1', 'paper_1', 'scissors_1'];
+
+
+srartBtn.addEventListener('click', general);
 
 
 
+function general() {
+  computerIMG.src = `images/rock_1.png`;
+  shakeHands();
+  const computerChoose = (() => {
+   return computerOptions[Math.floor(Math.random() * 3)];
+  })();
 
+  setTimeout(() => {
+    computerIMG.src = `images/${computerChoose}.png`;
+    console.log(computerIMG.src = `images/${computerChoose}.png`)
+  }, 1100);
+};
 
-srartBtn.addEventListener('click', shakeHands);
-
+function computerChoose () {
+  const randomNum = Math.floor(Math.random() * 3);
+  return randomNum;
+};
 
 
 
